eebf4442352c90957cf559b821db7eb1f8b496ed
Ethan Reed <52498418+ethanReed@users.noreply.github.com>
2020-05-04 14:44:24 -0500

    Add files via upload

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,18 +1,20 @@
 //Reedx500 Johall 004
 import java.util.Scanner;
 import java.io.*;
+import java.util.Random;
 
 public class HashTable <T extends Comparable<T> >{
 
-    private int firstHalfCount = 0;
-    private int secondHalfCount = 0;
-    private int firstHalfNodeSize = 0;
-    private int secondHalfNodeSize = 0;
+
 
     NGen[] hashT;
     private int length;
     private int count = 0;
+    private int offset = 1;
     int collisionCounter = 0;
+    Random r = new Random();
+    boolean keyword = false;
+
 
     public HashTable(){
         hashT = new NGen[101];
@@ -25,25 +27,23 @@ public class HashTable <T extends Comparable<T> >{
     }
     //part 1
     public String[] textScan (String fileName){
-
+        if (fileName.equals("keyword.txt")){
+            keyword = true;
+        }
         Scanner readFile = null;
         String s;
         String t;
         int i = 0;
         int count = 0;
 
-        System.out.println();
-        System.out.println("Attempting to read from file: " + fileName);
         try {
             readFile = new Scanner(new File(fileName));
         }
         catch (FileNotFoundException e) {
-            System.out.println("File: " + fileName + " not found");
             System.exit(1);
         }
 
-        System.out.println("Connection to file: " + fileName + " successful");
-        System.out.println();
+
         while(readFile.hasNext()) {
             t = readFile.next();
             count++;
@@ -60,7 +60,7 @@ public class HashTable <T extends Comparable<T> >{
             s = newReadFile.next();
             myString[i] = s;
             i++;
-            System.out.println("Token found: " + s);
+
         }
         System.out.println();
         System.out.println(count + " Tokens found");
@@ -72,8 +72,10 @@ public class HashTable <T extends Comparable<T> >{
     }
     //part 2
     public void add(T item) {
+        if (keyword == true){
+            int val = this.hashKeywords(item);
+        }
         int val = this.hash(item);
-        System.out.println(val);
         if(val < 0){
             val = val * -1;
         }
@@ -83,19 +85,21 @@ public class HashTable <T extends Comparable<T> >{
         if (hashT[val] == null) {
             hashT[val] = insert;
         }else{
-            NGen start = hashT[val];
-            if (start.getData().equals(item)) {
+            NGen start = hashT[val].getNext();
+            NGen tracer = hashT[val];
+            if (tracer.getData().equals(item)) {
                 return;
             }
-            while((start.getNext() != null)){
-                if (start.getData().equals(item)){
+            while((start != null)){
+                if (start.getData().equals(item)|| tracer.getData().equals(item)){
                     return;
                 }else {
                     start = start.getNext();
+                    tracer = tracer.getNext();
                 }
             }
             collisionCounter++;
-            start.setNext(insert);
+            tracer.setNext(insert);
         }
     }
     //part 3
@@ -151,6 +155,33 @@ public class HashTable <T extends Comparable<T> >{
         System.out.println("Short Chain: " + shortChain);
         System.out.println(tokenCount + " Tokens found");
     }
+
+    //old hash function
+/*private int hash(T key){
+        /*
+        This hash function works by converting a string into a character array. This is ideal because each
+        character has a corresponding number assigned to it so essential that means we can translate words into numbers.
+        This is very helpful because we can get into the index of the array.
+        To get a number for a word so, we start with a value of 0 and for every character, we add it to that total; finally
+        modding by the size of the array to give us an index.
+         */
+        /*
+        String k = (String) key;
+        int charToNum = 0;
+        int totalValOfS = 0;
+
+        char[] c = k.toCharArray();
+        for(int i = 0; i < c.length; i++){
+            c[i] = k.charAt(i);
+        }
+        for(int j = 0; j < c.length; j++){
+            charToNum = c[j];
+            totalValOfS += charToNum;
+        }
+        totalValOfS = totalValOfS % length;
+        return totalValOfS;
+    }
+    */
     //part 4
     private int hash(T key){
         /*
@@ -159,24 +190,58 @@ public class HashTable <T extends Comparable<T> >{
         This is very helpful because we can get into the index of the array.
         To get a number for a word so, we start with a value of 0 and for every character, we add it to that total; finally
         modding by the size of the array to give us an index.
+
+        **
+        This is an improvement from the function above because it uses more prime numbers. Prime numbers have a way of spreading out that data into
+        new arrays
+        **
+
+
          */
-        String k = (String) key;
-        int charToNum = 0;
+
+        String k = key.toString();
         int totalValOfS = 0;
         int hash = 7;
 
-        char[] c = k.toCharArray();
-        for(int i = 0; i < c.length; i++){
-            c[i] = k.charAt(i);
-        }
-        for(int j = 0; j < c.length; j++){
-            charToNum = c[j];
-            hash = (hash * 31) + charToNum;
+        for(int i = 0; i < k.length(); i++){
+            hash = (hash * 31) + k.charAt(i);
             totalValOfS += hash;
         }
         totalValOfS = totalValOfS % length;
         return totalValOfS;
     }
+    private int hashKeywords(T key){
+        /*
+        This hash function works by converting a string into a character array. This is ideal because each
+        character has a corresponding number assigned to it so essential that means we can translate words into numbers.
+        This is very helpful because we can get into the index of the array.
+        To get a number for a word so, we start with a value of 0 and for every character, we add it to that total; finally
+        modding by the size of the array to give us an index.
+         */
+
+        //This one is different than the previous one because since there are no duplicated values we can be more random
+        //with our placing. There is an incrementer that increases each time this function is called and it is used to create
+        //prime number. This is the best I could do. I tried for a couple hours and could not fully get the hash table to be collision free.
+        
+
+            //Create an array of random integers that are prime.
+
+            String k = key.toString();
+            int charToNum = 0;
+            int totalValOfS = 0;
+            int offsetSQRD = offset * offset;
+            int hash = offsetSQRD + offset + 41; //this formula will give a prime number for any integer put into it.
+        //the formula is x^2 + x + 41
+            offset++;
+
+            for(int i = 0; i < k.length(); i++){
+                hash = (k.charAt(i) * 31) + hash;
+                totalValOfS += hash;
+            }
+            totalValOfS = totalValOfS % length;
+            return totalValOfS;
+    }
+
     //Combines the methods, makes it simpilier.
     public void hashedTokens(String fName){
         String[] words = this.textScan(fName); //Create string array
@@ -187,10 +252,25 @@ public class HashTable <T extends Comparable<T> >{
     }
 
     public static void main(String[] args) {
-        HashTable x = new HashTable();
-        x.hashedTokens("keywords.txt");
+        HashTable keywords = new HashTable(501);
+        keywords.hashedTokens("keywords.txt");
+        keywords.display();
 
-        x.display();
+        HashTable gettsyburg  = new HashTable();
+        gettsyburg.hashedTokens("gettysburg.txt");
+        gettsyburg.display();
+
+        HashTable proverbs = new HashTable();
+        proverbs.hashedTokens("proverbs.txt");
+        proverbs.display();
+
+        HashTable that_bad = new HashTable();
+        that_bad.hashedTokens("that_bad.txt");
+        that_bad.display();
+
+        HashTable canterbury = new HashTable();
+        canterbury.hashedTokens("canterbury.txt");
+        canterbury.display();
 
         }
 }
